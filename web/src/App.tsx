f4e44be389cb2480cf509f17cedffd328fd8c8b0
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-01-22 20:48:42 -0300

    feat: implementation of new components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
-import { Habit } from "./components/Habit"
+import { Header } from "./components/Header"
+import { SummaryTable } from "./components/SummaryTable"
+
 import "./styles/global.css"
 
-function App() {
-  return <Habit completed={3} />
+export function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex w-full max-w-5xl flex-col gap-16 px-6">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
+  )
 }
-
-export default App
